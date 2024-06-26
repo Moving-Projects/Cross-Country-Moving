2b0d43fb47edff0d1ba3043fc118ae312003941e
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-06-26 20:48:02 +0300

    last update

--- app/sections/Services.tsx
+++ app/sections/Services.tsx
@@ -6,7 +6,7 @@ export default function Services () {
             <div className="flex flex-col lg:flex-row lg:justify-between mb-14 lg:mb-28">
                 <div className="relative">
                     <p className="text-xs lg:text-base mb-4 lg:mb-6 font-russo lg:absolute lg:-top-6 lg:-translate-y-full lg:left-0">WHAT CAN WE HELP YOU WITH</p>
-                    <h3 className="text-2xl lg:text-5xl font-russo">All Cross Country <span className="text-accent">Moving Services</span></h3>
+                    <h3 className="text-2xl lg:text-5xl font-russo">All Cross Country <span className="text-accent lg:block">Moving Services</span></h3>
                 </div>
                 <div className="lg:w-[41rem]">
                     <p className="text-sm lg:text-lg font-medium mt-6 lg:mt-0 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae mi nec tellus mattis accumsan. Ut quis est elit. Aliquam scelerisque sapien ultrices vehicula placerat. Ut aliquet ante mauris, et maximus odio mollis id. Integer ut dignissim risus.</p>
