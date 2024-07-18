d77466c8a2a4477fac7a0840f4b3559a024b796f
Moving-Projects <info@trekmovers.com>
2024-07-18 16:05:05 +0300

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: 'Cross Country Moving',
+export const metadata = {
+  metadataBase: new URL('https://www.crosscountrymoving.company/'),
+  title: "Cross Country Moving Company",
+  description: 'At Cross Country Moving Company, we are more than just movers; we are a team of dedicated professionals committed to making your moving experience as smooth and stress-free as possible.',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Cross Country Moving Company',
+    description: 'At Cross Country Moving Company, we are more than just movers; we are a team of dedicated professionals committed to making your moving experience as smooth and stress-free as possible.',
+    images: ['/Cross-Coutrny-Moving.png'],
+  },
+  openGraph: {
+    title: 'Cross Country Moving Company',
+    description: 'At Cross Country Moving Company, we are more than just movers; we are a team of dedicated professionals committed to making your moving experience as smooth and stress-free as possible.',
+    type: 'website',
+    url: 'https://www.crosscountrymoving.company/',
+    images: ['/Cross-Coutrny-Moving.png'],
+  },
 }
 
 export default function RootLayout({
